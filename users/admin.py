cefa1d2f072efd547d631ccd64bebf01cfa5e0ee
Rekha-2a2211 <bujji2a2211@gmail.com>
2026-04-28 08:53:22 +0000

    Enhance payments, admin UI, and front-end history sync

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from .models import UserProfile, LoginHistory
+
+admin.site.unregister(Group)
+admin.site.site_header = "Omni Platform Admin"
+admin.site.index_title = "Omni Platform Management"
+admin.site.site_title = "Omni Admin"
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
